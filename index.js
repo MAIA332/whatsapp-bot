e77c076e4ec91feab8e75fa35ab0cd20caafa1fd
MAIA332 <lk.maia0505@outlook.com>
2023-08-21 13:19:15 -0300

    get-started

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("bot heere");
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
+})
+
+client.initialize();
